dd93448e00fcc5ecebea1da9e5c30f253360a5ab
Chernyshov_Nikita <xpom5566@mail.ru>
2020-03-11 08:17:23 +0300

    feat: solved 36ms (100%) / 100%

--- main.py
+++ main.py
@@ -7,20 +7,19 @@ class Solution:
     """
         Do not return anything, modify nums in-place instead.
         """
-    all = []
+
     zeroes = []
+    allCounter = 0
 
     for index, val in enumerate(nums):
       if val == 0:
         zeroes.append(val)
       else:
-        all.append(val)
-
-    for index, val in enumerate(all):
-      nums[index] = val
+        nums[allCounter] = val
+        allCounter += 1
 
     for index, val in enumerate(zeroes):
-      nums[len(all) + index] = val
+      nums[allCounter + index] = val
 
 
 my = Solution()
@@ -31,5 +30,5 @@ my.moveZeroes(n0)
 
 print('ans', n0)
 
-# Runtime: 48 ms, faster than 74.43% of Python3 online submissions for Move Zeroes.
-# Memory Usage: 13.9 MB, less than 100.00% of Python3 online submissions for Move Zeroes.
+# Runtime: 36 ms, faster than 99.51% of Python3 online submissions for Move Zeroes.
+# Memory Usage: 13.8 MB, less than 100.00% of Python3 online submissions for Move Zeroes.
